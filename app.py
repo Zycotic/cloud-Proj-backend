6cfaf62c025198d6bbeee704bbcee31ae27c4717
Zycotic <mohamedharhash90@gmail.com>
2024-12-10 18:56:38 +0200

    installed gunicorn

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ def make_reservation():
     return jsonify({"message": "Reservation made successfully!"}), 201
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
